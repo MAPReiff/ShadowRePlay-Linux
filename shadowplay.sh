751c328e50ecf745c2a3b345b451540f3b802972
Mitchell Reiff <mapr323@gmail.com>
2020-04-22 13:35:22 -0400

    Fixed Time Var
    Fixed typo where the time in the command was not actually calling the var

--- shadowplay.sh
+++ shadowplay.sh
@@ -12,7 +12,7 @@ RESOLUTION="1920x1080"
 # Save time hh:mm:ss
 TIME="00:05:00"
 # Save location
-VIDEOFOLDER="${HOME}/Videos"
+VIDEOFOLDER="${HOME}/Videos/"
 
 BASEDIR=$(dirname "$0")
 
@@ -21,7 +21,7 @@ FILE="${VIDEOFOLDER}/replay-${DATE}.mp4"
 
 ${INSTALL} -f pulse -i 0 -f x11grab -framerate ${FPS} -s ${RESOLUTION} -i :0.0 -c:v h264_nvenc -preset:v llhq -profile:v high -pix_fmt nv12 -b:v 15M -acodec aac /tmp/out.mp4 ||
 
-ffmpeg -sseof -TIME -i /tmp/out.mp4 -vcodec copy -acodec copy "$FILE" -loglevel quiet &&
+ffmpeg -sseof -${TIME} -i /tmp/out.mp4 -vcodec copy -acodec copy "$FILE" -loglevel quiet &&
 
 rm /tmp/out.mp4 &&
 
