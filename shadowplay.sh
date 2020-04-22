61ce33c35531f6ec35d0bd6f5dcfc86698ee00f4
jgerega107 <jakegerega107@gmail.com>
2020-04-22 18:46:45 -0400

    Added a check to see if video directory exists, made script override tmp recording, and added audio recording.

--- shadowplay.sh
+++ shadowplay.sh
@@ -13,13 +13,20 @@ RESOLUTION="1920x1080"
 TIME="00:05:00"
 # Save location
 VIDEOFOLDER="${HOME}/Videos/"
+#Check to see if directory exists, else make location if it doesnt exist
+
+if [ -e "${VIDEOFOLDER}" ]; then
+    echo "Video directory already exists."
+else
+    mkdir ${VIDEOFOLDER}
+fi
 
 BASEDIR=$(dirname "$0")
 
 DATE=$(date +%Y-%d-%m-%H:%M:%S)
 FILE="${VIDEOFOLDER}/replay-${DATE}.mp4"
 
-${INSTALL} -f pulse -i 0 -f x11grab -framerate ${FPS} -s ${RESOLUTION} -i :0.0 -c:v h264_nvenc -preset:v llhq -profile:v high -pix_fmt nv12 -b:v 15M -acodec aac /tmp/out.mp4 ||
+${INSTALL} -f pulse -i default -f x11grab -framerate ${FPS} -s ${RESOLUTION} -i :0.0 -c:v h264_nvenc -preset:v llhq -profile:v high -pix_fmt nv12 -b:v 15M -acodec aac /tmp/out.mp4 -y||
 
 ffmpeg -sseof -${TIME} -i /tmp/out.mp4 -vcodec copy -acodec copy "$FILE" -loglevel quiet &&
 
